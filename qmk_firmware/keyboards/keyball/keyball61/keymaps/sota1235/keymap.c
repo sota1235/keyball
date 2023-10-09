8d26ece9f3b858e6644daf53d2ab65ae7a319409
sota1235 <sota1235@gmail.com>
2023-10-09 21:58:40 +0900

    modify: update key and disable via

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/sota1235/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/sota1235/keymap.c
@@ -24,10 +24,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
-    KC_DEL   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_INT3  ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_INT3  ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
     MO(1)    , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-    _______  , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),    KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______ , KC_RALT  , KC_PSCR
+    _______  , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LGUI),LT(2,KC_SPC),LT(3,KC_RGUI),      KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______  , KC_RALT  , KC_PSCR
   ),
 
   [1] = LAYOUT_universal(
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , KC_7     , KC_8     , KC_9     , _______  ,                                  _______  , _______  , _______  , _______  , _______  , KC_F12   ,
     _______  , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGUP  , _______  , _______  , _______  , KC_BTN3  , _______  ,
     _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,             KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , KC_0     , KC_DOT   , KC_LNG2  , _______  , KC_LNG1  ,             KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
--- qmk_firmware/keyboards/keyball/keyball61/keymaps/sota1235/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/sota1235/rules.mk
@@ -4,7 +4,8 @@ RGBLIGHT_ENABLE = no
 OLED_ENABLE = yes
 
 # REMAP使わないので無効化する
-VIA_ENABLE = yes
+# VIA_ENABLE = yes
+VIA_ENABLE = no
 
 # macOSメディアキー等を有効化する
 EXTRAKEY_ENABLE = yes
