f97a3593130862cf230a19b7a036b729ade7b34c
sota1235 <sota1235@gmail.com>
2023-10-09 22:05:21 +0900

    modify: enable via

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/sota1235/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/sota1235/rules.mk
@@ -3,9 +3,8 @@ RGBLIGHT_ENABLE = no
 
 OLED_ENABLE = yes
 
-# REMAP使わないので無効化する
-# VIA_ENABLE = yes
-VIA_ENABLE = no
+VIA_ENABLE = yes
+# VIA_ENABLE = no
 
 # macOSメディアキー等を有効化する
 EXTRAKEY_ENABLE = yes
