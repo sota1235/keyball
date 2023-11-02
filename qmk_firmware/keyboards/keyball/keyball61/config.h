9797bc284ad39b320986899f12db25bcec9b5e08
sota1235 <sota1235@gmail.com>
2023-11-02 12:20:15 +0900

    modify: change AUTO_MOUSE_TIME

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -71,3 +71,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Automatic Mouse Layer
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 4
+#define AUTO_MOUSE_TIME 450 // default: 650
