e00947d15d9dd56cb7c9e91208f914b01bb2b58b
sota1235 <sota1235@gmail.com>
2024-10-17 16:58:14 +0900

    modify: change AUTO_MOUSE_TIME

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -71,4 +71,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Automatic Mouse Layer
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 4
-#define AUTO_MOUSE_TIME 450 // default: 650
+// マウスが止まってから元のレイヤに戻るまでの時間
+#define AUTO_MOUSE_TIME 350 // default: 650
